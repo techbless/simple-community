8ef20e623cd71f0c8cac20d82eadb3f145eba099
Yunbin Chang <yunbin@hansung.ac.kr>
2020-04-06 02:27:46 +0900

    Dont use form tag and Redirect after uploading an article

--- frontend/src/pages/NewPost.tsx
+++ frontend/src/pages/NewPost.tsx
@@ -1,37 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 import Header from '../component/Header';
 import '../styles/NewPost.css';
+import api from '../api';
 
-function uploadArticle() {
+async function uploadArticle(title: string, content: string) {
+    await api.post(
+        '/articles', {
+            title: title,
+            content: content
+        }
+    )
+}
 
+function renderRedirect(uploaded: boolean) {
+    if (uploaded) return <Redirect to='/' />
 }
 
 function NewPost() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const [uploaded, setUploaded] = useState(false);
+
     return (
         <div>
+            { renderRedirect(uploaded) }
+            
             <Header />
             <div className="editor">
                 <h2>새로운 글</h2><br/>
-                <form method="post" onSubmit={uploadArticle}>
-                    <input 
-                        placeholder="글제목"
-                        className="text-input title-input"
-                        type="text"
-                        name="title"
-                        id="title"
-                    /><br/>
+                
+                <input 
+                    placeholder="글제목"
+                    className="text-input title-input"
+                    type="text"
+                    name="title"
+                    id="title"
+                    value={title}
+                    onChange={(e) => {setTitle(e.target.value)}}
+                /><br/>
 
-                    <textarea 
-                        placeholder="여기에 글을 작성하세요."
-                        className="text-input"
-                        name="content"
-                        id="content"
-                        cols={30}
-                        rows={12}>
-                    </textarea><br/>
+                <textarea 
+                    placeholder="여기에 글을 작성하세요."
+                    className="text-input"
+                    name="content"
+                    id="content"
+                    cols={30}
+                    rows={12}
+                    value={content}
+                    onChange={(e) => {setContent(e.target.value)}}
+                /><br/>
 
-                    <input type="submit" className="btn btn-primary add-btn" value="추가" />
-                </form>
+                <Button className="add-btn" onClick={
+                    async () => {
+                        await uploadArticle(title, content);
+                        setUploaded(true);
+                    }
+                }>추가</Button>
+               
             </div>
         </div>
     );
