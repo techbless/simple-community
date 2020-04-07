5510d58b581aa0009e4a011d3c71b0821356bd00
Yunbin Chang <yunbin@hansung.ac.kr>
2020-04-07 19:35:53 +0900

    Add GET /comments/:id function for one comment

--- backend/src/controllers/comment.ts
+++ backend/src/controllers/comment.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import CommentService from '../services/comment';
 import ArticleService from '../services/article';
-import Article from '../models/article';
 
 class CommentController {
   public getComments = async (req: Request, res: Response) => {
@@ -16,18 +15,32 @@ class CommentController {
     res.json(comments);
   }
 
+  public getComment = async (req: Request, res: Response) => {
+    const commentId: number = + req.params.commentId;
+
+    const comment = await CommentService.getComment(commentId);
+    if(comment) res.status(201).json(comment);
+    else res.sendStatus(404);
+  }
+
   public createComment = async (req: Request, res: Response) => {
     const articleId: number = + req.params.articleId;
     const message: string = req.body.comment;
     const writterId: number = req.user!.userId;
 
     const comment = await CommentService.createComment(articleId, message, writterId);
-    res.status(201).json(comment);
+
+    if(comment.commentId) {
+      const result = await CommentService.getComment(comment.commentId);
+      res.status(201).json(result);
+    } else {
+      res.sendStatus(500);
+    }
   }
 
   public deleteComment = async (req: Request, res: Response) => {
-    const articleId: number = +req.params.articleId;
-    const commentId: number = +req.params.commentId;
+    const articleId: number = + req.params.articleId;
+    const commentId: number = + req.params.commentId;
     const userId: number = req.user!.userId;
 
     const isOwner: boolean = await CommentService.checkOwner(userId, commentId);
